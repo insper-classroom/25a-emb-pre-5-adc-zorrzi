b69711db73d85f2053d8009ab8719a311f2b61bd
eduardo <eduardovz@al.insper.edu.br>
2025-04-07 10:43:02 -0300

    feat: exe1

--- exe1/main.c
+++ exe1/main.c
@@ -12,15 +12,24 @@
 void adc_1_task(void *p) {
     adc_init();
     adc_gpio_init(27);
+    adc_gpio_init(26);
 
     // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
     const float conversion_factor = 3.3f / (1 << 12);
 
-    uint16_t result;
+    uint16_t result1;
+    uint16_t result2;
+
     while (1) {
         adc_select_input(1); // Select ADC input 1 (GPIO27)
-        result = adc_read();
-        printf("voltage 1: %f V\n", result * conversion_factor);
+        result1 = adc_read();
+
+        adc_select_input(0);
+        result2 = adc_read();
+
+        printf("voltage 1: %f V\n", result1 * conversion_factor);
+        printf("voltage 0: %f V\n", result2 * conversion_factor);
+
 
         // CÓDIGO AQUI
 
